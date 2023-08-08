eee849e644f71393ab2afaad79fa52c8bcc4dc32
samimassoud <137684706+samimassoud@users.noreply.github.com>
2023-08-08 14:47:46 +0300

    Add files via upload

---
+++ src/test/java/mysakann/AcceptanceTest.java
@@ -0,0 +1,20 @@
+package mysakann;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "use_cases/provided/tenants.feature",
+    plugin = {"summary", "html:target/cucumber/wikipedia.html"},
+    monochrome = true,
+    snippets = SnippetType.CAMELCASE,
+    glue = "mysakann"
+)
+
+public class AcceptanceTest {
+
+}
